2b400010db65a2daac7b5d20f5dbeb30d4728885
Izaias Lima <izaiaslima356@gmail.com>
2025-05-03 16:30:04 -0300

    chore: remove unused Prisma environment configuration from Vite test setup

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,8 +4,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
 	plugins: [tsconfigPaths()],
 	test: {
-		environment: "prisma",
 		environmentMatchGlobs: [["src/infra/http/controllers/**", "prisma"]],
-		exclude: ["**/node_modules/**", "**/dist/**"],
 	},
 });
